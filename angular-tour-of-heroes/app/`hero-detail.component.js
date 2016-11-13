e6d2f3ffc02c27d22e5a3532f2e2992a7352b828
Sagir <sagir.sagir@gmail.com>
2016-11-13 19:28:23 +0530

    multiple components

---
+++ angular-tour-of-heroes/app/`hero-detail.component.js
@@ -0,0 +1,8 @@
+"use strict";
+var hero_1 = require('./hero');
+Class;
+HeroDetailComponent;
+{
+    hero: hero_1.Hero;
+}
+//# sourceMappingURL=`hero-detail.component.js.map
--- angular-tour-of-heroes/app/app.component.ts
+++ angular-tour-of-heroes/app/app.component.ts
@@ -25,7 +25,9 @@ const HEROES: Hero[] = [
 	template:`
 			 <h2>My Heroes</h2>
 			<ul class="heroes">
-			  <li *ngFor = "let hero of heroes" (click) = "onSelect(hero)">
+			  <li *ngFor = "let hero of heroes" 
+			  		[class.selected]="hero === selectedHero"
+			  		(click) = "onSelect(hero)">
 			    <span class="badge">{{hero.id}}</span> {{hero.name}}
 			  </li>
 			</ul>
@@ -90,12 +92,10 @@ const HEROES: Hero[] = [
 
 })
 
-
 export class AppComponent {
   title = 'Tour of Heroes';
   heroes = HEROES;
   selectedHero: Hero;
-
 	onSelect(hero: Hero): void{
 		this.selectedHero = hero;
 	}
